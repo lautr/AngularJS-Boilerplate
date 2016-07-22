d7f3d2c651fe3e287dc728fa2364065b373780d9
Johannes lauter <hannes@lautr.com>
2016-07-22 10:24:22 +0000

    chore(versions): bumping versions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 /**
  * Created by johannes on 17.07.15.
  */
-/**
- * Created by johannes on 28.04.15.
- */
 define(['angularAMD', 'jquery'], function (angularAMD) {
   var app = angular.module("boilerplateApp", []);
 
